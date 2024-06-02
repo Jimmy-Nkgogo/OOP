a1e0fda5428f95bd944bfa00585a9de4ce9aff03
Jimmy <nkgogojimmy@gmail.com>
2024-06-02 22:16:42 +0200

    finishd first video on inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inheritance is a relationship between two classes that allow one to inherit code
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+        }
+    }
+}
---
+++ IntroToClasses/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToClasses
+{
+    internal class Person
+    {
+        public string Name;
+
+        public void Introduce(string otherName)
+        {
+            Console.WriteLine($"Hi {otherName}, I am {Name}");
+        }
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+            return person;
+        }
+    }
+}
