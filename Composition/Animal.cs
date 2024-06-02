f85e5dd2d1c60792723f27a9d8f175e905ceda36
Jimmy <nkgogojimmy@gmail.com>
2024-06-02 23:43:58 +0200

    Learned about abstraction

---
+++ Composition/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    public abstract class Animal
+    {
+        public string Name;
+        public void setName(string name)
+        {
+            Name = name;
+        }
+        public abstract void MakeSound();
+    }
+    public class Bird : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Chu chu");
+        }
+        public void displayInfo()
+        {
+            Console.WriteLine();
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof");
+        }
+        public void displayInfo()
+        {
+            Console.WriteLine();
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+        public void displayInfo()
+        {
+            Console.WriteLine();
+        }
+    }
+
+}
---
+++ Composition/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Dog dog = new Dog();
+            dog.Name = "Rex";
+            Cat cat = new Cat();
+            cat.Name = "Mohawk";
+
+            dog.displayInfo();
+            dog.MakeSound();
+            cat.displayInfo();
+            dog.MakeSound();
+             */
+        }
+    }
+    class Student
+    {
+        private int studentNumber;
+        private string name;
+        private string surname;
+        public Student()
+        {
+            studentNumber = 0;
+            name = null;
+            surname = null;
+        }
+        public void setStudent(int num, string n, string s)
+        {
+            studentNumber = num;
+            name = n;
+            surname = s;
+        }
+        public void displayStudent()
+        {
+            Console.WriteLine("\n" + studentNumber + " " + name + " " + surname);
+        }
+    }
+}
